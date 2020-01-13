d8378b568a970bfde8eb224cd5a940a8261862dd
Macostik <y.granchenko@simcord.com>
2020-01-13 12:13:03 +0200

    comment presenting method

--- MVVM-AC.xctemplate/Fullcode/___FILEBASENAME___Coordinator.swift
+++ MVVM-AC.xctemplate/Fullcode/___FILEBASENAME___Coordinator.swift
@@ -14,8 +14,8 @@ class ___FILEBASENAMEASIDENTIFIER___: BaseSceneCoordinator<Void> {
         return Observable.just(())
     }
     
-    @discardableResult private func present<#Class#>Scene() -> Observable<Void> {
-        let <#Class#>Coordinator = <#Class#>SceneCoordinator(window: window, dependencies: dependencies)
-        return coordinate(to: <#Class#>Coordinator)
-    }
+//    @discardableResult private func present<#Class#>Scene() -> Observable<Void> {
+//        let <#Class#>Coordinator = <#Class#>SceneCoordinator(window: window, dependencies: dependencies)
+//        return coordinate(to: <#Class#>Coordinator)
+//    }
 }
