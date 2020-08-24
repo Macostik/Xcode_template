2c06ee981dd7810b2281a96f21563b4a975ad881
Macostik <yura-granchenko@yandex.ru>
2020-08-24 12:45:51 +0300

    refactor coordinator

--- MVVM-AC.xctemplate/XIB file/___FILEBASENAME___Coordinator.swift
+++ MVVM-AC.xctemplate/XIB file/___FILEBASENAME___Coordinator.swift
@@ -4,18 +4,16 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class ___FILEBASENAMEASIDENTIFIER___: BaseSceneCoordinator<Void> {
+class ___FILEBASENAMEASIDENTIFIER___: BaseSceneCoordinator<TransferDataType> {
     
-    override func start() -> Observable<Void> {
-        let viewModel = ___VARIABLE_productName:identifier___ViewModel(dependencies: dependencies)
+    override func start() -> Observable<TransferDataType> {
+        let viewModel = ___VARIABLE_productName:identifier___ViewModel(dependency: dependency)
         let viewController = ___VARIABLE_productName:identifier___ViewController.instantiate(with: viewModel)
         let navigationController = window.rootViewController as? UINavigationController
         navigationController?.pushViewController(viewController, animated: false)
-        return Observable.just(())
+        viewModel.present.subscribe(onNext: { [unowned self] scene in
+            self.presentCoordinator(scene.rawValue)
+        }).disposed(by: disposeBag)
+        return Observable.just(.empty)
     }
-    
-//    @discardableResult private func present<#Class#>Scene() -> Observable<Void> {
-//        let <#Class#>Coordinator = <#Class#>SceneCoordinator(window: window, dependencies: dependencies)
-//        return coordinate(to: <#Class#>Coordinator)
-//    }
 }
